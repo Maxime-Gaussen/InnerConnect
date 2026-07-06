de6a2f392b1afa1fe4961f869595a701f925f38c
Maxime-Gaussen <maximegaussen34160@gmail.com>
2026-07-06 13:27:39 +0200

    Delete model directory

--- model/user.js
+++
@@ -1,6 +0,0 @@
-import { AbstractModel } from "./AbstractModel.js";
-
-export class User extends AbstractModel {
-    table = "User";
-    colones = ["nom", "prenom", "age", "email", "password"];
-}
